a31af08757a35dec5e5fdfc68a8ce442bd3aef81
James Portune <Jayms.Portune@gmail.com>
2019-07-22 17:44:46 -0700

    Saving Progress, getting select and options pushed through handlebars for requirements

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@ let obKeyword = [];
 let obHorns = [];
 let fileName = '../data/page-1.json'
 
-
-
 const MyHorns = function(image_url, title, description, keyword, horns){
     this.image_url = image_url;
     this.title = title;
@@ -15,28 +13,59 @@ const MyHorns = function(image_url, title, description, keyword, horns){
     this.horns = horns;
     obHorns.push(this.horns);
 }
+
 MyHorns.readJson = function(filename){
     $.get(filename,'json').then(myHornsJson => {
         myHornsJson.forEach(horn => {
             new MyHorns(horn.image_url, horn.title, horn.description, horn.keyword, horn.horns);
-            let target = $('#imgTemp').html();
-            toHtmlImg(horn.image_url, horn.title, horn.description, horn.keyword, horn.horns, target);
+        });
+        obName = removeDuplicates(obName);
+        obKeyword = removeDuplicates(obKeyword);
+        obHorns = removeDuplicates(obHorns);
+        obHorns.sort(function(a,b){return a-b});
+        obKeyword.forEach(horn => {
+            toHtmlOp(horn);
+        });
+        myHornsJson.forEach(horn => {
+            toHtmlImg(horn.image_url, horn.title, horn.keyword);
         });
     });
 };
 
-function toHtmlImg(image_url, title, description, keyword, horns, target) {
+function toHtmlOp(keyword){
+    target = $('#opTemp').html();
+    let context = {
+        "opClass": keyword,
+        "opText": keyword
+    };
+    console.log(keyword);
+}
+
+function toHtmlImg(image_url, title, keyword) {
+    target = $('#imgTemp').html();
     let context = { 
         "imgKeyword": keyword, 
         "imgTitle": title, 
-        "imgSrc": image_url,
-        "opText": keyword,
-        "opClass": keyword,
-        "opHorns": horns
+        "imgSrc": image_url
     };
     let templateScript = Handlebars.compile(target);
     let html = templateScript(context);
     $(document.body).append(html);
+}
+
+var removeDuplicates = function(arr) {
+    var sortedArray = arr.sort();
+    var len = sortedArray.length - 1;
+    var newArr = [];
+    if (len >= 0) {
+        for (var i = 0; i < len; i++) {
+            if (sortedArray[i] !== sortedArray[i + 1]) {
+                newArr.push(sortedArray[i]);
+            }
+        }
+        newArr.push(sortedArray[len]);
+    }
+    return newArr;
 }
 
 $(()=>MyHorns.readJson(fileName));
--- js/app2.js
+++ js/app2.js
@@ -1,28 +1,8 @@
-
-MyHorns.prototype.tohtml=function(){
-  let $target=$('#handlebar').html();
-  let $source=Handlebars.compile($target);
-  return $source(this);
-};
-
 $('select').on('change',function(){
   $('div').hide();
   console.log($(this).val());
   $('div.' + $(this).val()).show();
 });
-
-MyHorns.prototype.render = function(keyword, hornsOp) {
-  minOp(keyword);
-  $('#filterName').append($('<option>', {text:keyword}).attr('class',this.keyword));
-  $('main').append('<div class="clone"></div>');
-  let hornClone=$('div[class="clone"]');
-  let hornHtml=$('#photo-template').html();
-  hornClone.html(hornHtml);
-  hornClone.find('h2').text(this.title);
-  hornClone.find('img').attr('src',this.image_url);
-  hornClone.removeClass('clone');
-  hornClone.attr('class',this.keyword);
-}
 
 $('#one').on('click',function(){
   fileName = '../data/page-1.json'
@@ -42,8 +22,3 @@ $('#two').click(function(){
   allKeys=[];
   $(()=>MyHorns.readJson(fileName));
 });
-
-function minOp(keyword) {
-  $('option.' + keyword).remove();
-}
-
